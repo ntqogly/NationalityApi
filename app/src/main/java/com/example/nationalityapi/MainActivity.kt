1a392eb2bfc6f11eff19ae5ebdd052e909007d3a
Qarapapax <aldunsar@yahoo.com>
2023-03-20 00:22:49 +0300

    Probability formated do percentage. NightMode off.

--- app/src/main/java/com/example/nationalityapi/MainActivity.kt
+++ app/src/main/java/com/example/nationalityapi/MainActivity.kt
@@ -1,16 +1,13 @@
 package com.example.nationalityapi
 
 import android.os.Bundle
-import android.os.SystemClock
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.nationalityapi.databinding.ActivityMainBinding
 import com.example.nationalityapi.network.ApiFactory
-import com.google.gson.Gson
 import kotlinx.coroutines.launch
-import org.json.JSONObject
-import java.nio.charset.Charset
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,6 +15,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -28,25 +26,17 @@ class MainActivity : AppCompatActivity() {
 
         binding.button.setOnClickListener {
             lifecycleScope.launch {
-                buttonLoad()
+                loadNationality()
             }
         }
     }
 
 
-    private suspend fun buttonLoad() {
-        binding.chronometer.base = SystemClock.elapsedRealtime()
-        binding.chronometer.start()
-        binding.button.isEnabled = false
-        loadNationality()
-        binding.chronometer.stop()
-        binding.button.isEnabled = true
-    }
-
     private suspend fun loadNationality() {
-        val list = ApiFactory.getApiService().getNationality()
-        binding.apply {
-            adapter.submitList(list.country)
-        }
+        binding.button.isEnabled = false
+        val myName = binding.etName.text.toString()
+        val list = ApiFactory.getApiService().getNationality(myName)
+        adapter.submitList(list.country)
+        binding.button.isEnabled = true
     }
 }
--- app/src/main/java/com/example/nationalityapi/network/ApiFactory.kt
+++ app/src/main/java/com/example/nationalityapi/network/ApiFactory.kt
@@ -1,30 +1,25 @@
 package com.example.nationalityapi.network
 
-import com.google.gson.GsonBuilder
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-class ApiFactory {
+object ApiFactory {
 
-    companion object {
-        private const val BASE_URL = "https://api.nationalize.io"
+    private const val BASE_URL = "https://api.nationalize.io"
 
-        fun getApiService(): ApiService {
-            val interceptor = HttpLoggingInterceptor()
-            interceptor.level = HttpLoggingInterceptor.Level.BODY
-            val client = OkHttpClient.Builder().addInterceptor(interceptor).build()
+    fun getApiService(): ApiService {
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.level = HttpLoggingInterceptor.Level.BODY
+        val client = OkHttpClient.Builder().addInterceptor(interceptor).build()
 
-            val gson = GsonBuilder().setLenient().create()
-
-            val retrofit = Retrofit.Builder()
-                .addConverterFactory(GsonConverterFactory.create(gson))
-                .baseUrl(BASE_URL)
-                .client(client)
-                .build()
-
-            return retrofit.create(ApiService::class.java)
-        }
+        val retrofit = Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(BASE_URL)
+            .client(client)
+            .build()
+        return retrofit.create(ApiService::class.java)
     }
+
 }
